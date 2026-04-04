e66e883e03812b8205258b6513b8ca94887d4e34
Felix Andre <andre_12_aries@hotmail.com>
2026-04-04 10:18:54 -0500

    Cambios testing

--- scripts/stages.js
+++ scripts/stages.js
@@ -1,13 +1,9 @@
 import http from "k6/http"
 
 export let options = {
-    stages: [
-        { duration: "30s", target: 20 },
-    
-        { duration: "1m", target: 20 },
-    
-        { duration: "30s", target: 0 },
-    ],
+    vus: 10, 
+    duration: '10s'
+
 }        
 
 export default function () {
